b6affdc7f8d806a186d78aef044dd85e29c87666
Marcin Garnowski <47816425+Trawirr@users.noreply.github.com>
2023-01-21 13:20:01 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+using System.Management;
+
+namespace escaperoomforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.Black;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            //            this.ClientSize = new System.Drawing.Size(1500, 600);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Escaperoom";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            //            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.Size = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Size;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            System.Console.WriteLine(this.pictureBox1.Size);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace escaperoomforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            UsbNotification.RegisterUsbDeviceNotification(this.Handle);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == UsbNotification.WmDevicechange)
+            {
+                switch ((int)m.WParam)
+                {
+                    case UsbNotification.DbtDeviceremovecomplete:
+                        System.Console.WriteLine("USB removed");
+                        this.pictureBox1.ImageLocation = "C:/Users/gtraw/OneDrive/Pulpit/app/media/codes.jpg";
+                        break;
+                    case UsbNotification.DbtDevicearrival:
+                        System.Console.WriteLine("USB injected");
+                        this.pictureBox1.ImageLocation = "C:/Users/gtraw/OneDrive/Pulpit/app/media/wiezienie komp gif.gif";
+                        System.Console.WriteLine(this.pictureBox1.Size);
+                        System.Console.WriteLine(this.pictureBox1.Image.Size);
+                        break;
+                }
+            }
+        }
+    }
+}
